5a726c8620d99fd3fd0a248cef0c0c902ca0103d
Tran Van Hung <hungtranvan1110@gmail.com>
2026-05-07 15:23:05 +0700

    hihi

---
+++ public/chess.js
@@ -0,0 +1,5 @@
+// Chess.js v1.0.0 (minified, ES5)
+// https://github.com/jhlywa/chess.js
+// Only core logic for browser, no module.exports
+// ... (minified code will be inserted here) ...
+// For brevity, will insert the real minified code in the next step.
